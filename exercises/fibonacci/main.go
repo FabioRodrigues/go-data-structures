b23481af64e5249ad9afb486fef898c5da214e54
Fabio Rodrigues <fabio.rodrigues018@gmail.com>
2020-01-13 10:22:35 -0300

    add: mamorization in the fibonnaci calculation

--- exercises/fibonacci/main.go
+++ exercises/fibonacci/main.go
@@ -7,6 +7,9 @@ func main() {
 	fmt.Println(fib)
 }
 
+var isInCache map[int]bool = map[int]bool{}
+var cache map[int]int = map[int]int{}
+
 func fibonacciNotRecursive(term int) int {
 	if term == 1 || term == 2 {
 		return term - 1
@@ -22,8 +25,19 @@ func fibonacciNotRecursive(term int) int {
 }
 
 func fibonacciRecursive(term int) int {
+	if isInCache[term] {
+		fmt.Printf("item %d is in cache\n", term)
+		return cache[term]
+	}
+	fmt.Printf("item %d is not in cache\n", term)
 	if term == 0 || term == 1 {
+		isInCache[term] = true
+		cache[term] = term
 		return term
 	}
-	return fibonacciRecursive(term-1) + fibonacciRecursive(term-2)
+	result := fibonacciRecursive(term-1) + fibonacciRecursive(term-2)
+	isInCache[term] = true
+	cache[term] = result
+
+	return result
 }
